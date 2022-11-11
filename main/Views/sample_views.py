409f7d0b65d5ca1f0e6c80ba46f904316cf278b7
Vahid <vd1371@gmail.com>
2022-11-11 08:59:01 +0800

    creating the base for adding REST apis

--- main/Views/sample_views.py
+++ main/Views/sample_views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from . import forms
+from .. import forms
 
 # Create your views here.
 def home_view(request):
